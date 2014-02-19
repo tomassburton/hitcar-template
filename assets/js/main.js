487eb37329ce261f036ae6a2302820c471088103
tomassburton <tomas.nahl@gmail.com>
2014-02-19 20:51:54 +0100

    v1.2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,28 @@
-$('#slider').bxSlider({
+var $r = jQuery.noConflict();
+$r(document).ready(function() {
+	$(".modal").fancybox({
+		maxWidth	: 900,
+		maxHeight	: 800,
+		fitToView	: false,
+		width		: '80%',
+		height		: '80%',
+		autoSize	: true,
+		closeClick	: false,
+		openEffect	: 'none',
+		closeEffect	: 'none'
+	});
+	$(".foto").fancybox({
+		transitionIn	:	'none',
+		transitionOut	:	'none',
+		speedIn			:	300, 
+		speedOut		:	200, 
+		overlayShow		:	false
+	});
+});
+
+
+var $j = jQuery.noConflict();
+$j('#slider').bxSlider({
   captions: true,
   auto: true
 });
