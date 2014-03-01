84e4249def4812ee3ce13da4776626b4f6aaa6a1
tomassburton <tomas.nahl@gmail.com>
2014-03-01 16:54:07 +0100

    v1.3

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,9 +20,33 @@ $r(document).ready(function() {
 	});
 });
 
-
 var $j = jQuery.noConflict();
 $j('#slider').bxSlider({
   captions: true,
   auto: true
+});
+
+var $ = jQuery.noConflict();
+$('ul.tabs').each(function(){
+  var $active, $content, $links = $(this).find('a');
+  $active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
+  $active.addClass('active');
+  $content = $($active.attr('href'));
+
+  $links.not($active).each(function () {
+    $($(this).attr('href')).hide();
+  });
+
+  $(this).on('click', 'a', function(e){
+    $active.removeClass('active');
+    $content.hide();
+
+    $active = $(this);
+    $content = $($(this).attr('href'));
+
+    $active.addClass('active');
+    $content.show();
+
+    e.preventDefault();
+  });
 });
